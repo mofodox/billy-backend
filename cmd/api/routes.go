64d54357e6734bf17edfceb0e5563e1477ad2da3
mofodox <khairulakmal.dev@gmail.com>
2022-11-08 09:21:40 +0800

    feat: use chi built in middlewares

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,9 @@ func (app application) setupRoutes() *chi.Mux {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger)
+	router.Use(middleware.CleanPath)
+	router.Use(middleware.AllowContentType("application/json"))
+	router.Use(middleware.Heartbeat("/api/v1"))
 
 	router.Route("/api/v1", func(r chi.Router) {
 		r.Get("/healthcheck", app.healthcheckHandler)
